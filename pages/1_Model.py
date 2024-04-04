67dd3bdcf9b71b308af9ea1343b8c7c5cac8eef7
chen.s.g <csg800@126.com>
2024-04-04 21:35:25 +0800

    集成书生·浦语大模型配置

--- pages/1_Model.py
+++ pages/1_Model.py
@@ -1,15 +1,28 @@
 import streamlit as st
+from lagent.actions import ArxivSearch
+from lagent.agents.internlm2_agent import INTERPRETER_CN, META_CN, PLUGIN_CN
 
 model_cfg = {
-    'model_name': 'internLM2',
-    'model_path': '/data/models/internLM2',
-    'system_prompt': '',
-    'plugin_prompt': '',
+    'model_name': 'internlm2-chat-1_8b',
+    'model_path': '/root/models/Shanghai_AI_Laboratory/internlm2-chat-1_8b',
+
     'temperature': 0.7,
     'max_tokens': 256,
     'top_p': 1,
     'frequency_penalty': 0,
-    'presence_penalty': 0
+    'presence_penalty': 0,
+
+    'prompt_meta': META_CN,
+    'prompt_da': INTERPRETER_CN,
+    'prompt_plugin': PLUGIN_CN,
+
+    'user': [],
+    'assistant': [],
+    'session_history': [],
+
+    'plugin_map': [
+        ArxivSearch(),
+    ]
 }
 
 if 'model' not in st.session_state:
@@ -26,16 +39,16 @@ max_tokens = st.text_input('最大令牌数', value=st.session_state['model']['m
 top_p = st.text_input('置信度', value=st.session_state['model']['top_p'], max_chars=None, key=None, type='default', placeholder = '请输入置信度')
 frequency_penalty = st.text_input('频率惩罚', value=st.session_state['model']['frequency_penalty'], max_chars=None, key=None, type='default', placeholder = '请输入频率惩罚')
 presence_penalty = st.text_input('位置惩罚', value=st.session_state['model']['presence_penalty'], max_chars=None, key=None, type='default', placeholder = '请输入位置惩罚')
-system_prompt = st.text_area('系统提示词', value=st.session_state['model']['system_prompt'], max_chars=None, key=None, placeholder = '请输入系统提示词')
-plugin_prompt = st.text_area('插件提示词', value=st.session_state['model']['plugin_prompt'], max_chars=None, key=None, placeholder = '请输入插件提示词')
+prompt_meta = st.text_area('系统提示词', value=st.session_state['model']['prompt_meta'], max_chars=None, key=None, placeholder = '请输入系统提示词')
+prompt_plugin = st.text_area('插件提示词', value=st.session_state['model']['prompt_plugin'], max_chars=None, key=None, placeholder = '请输入插件提示词')
 
 saved = st.button('保存设置')
 
 if saved:
     st.session_state['model']['model_name'] = model_name
     st.session_state['model']['model_path'] = model_path
-    st.session_state['model']['system_prompt'] = system_prompt
-    st.session_state['model']['plugin_prompt'] = plugin_prompt
+    st.session_state['model']['prompt_meta'] = prompt_meta
+    st.session_state['model']['prompt_plugin'] = prompt_plugin
     st.session_state['model']['temperature'] = temperature
     st.session_state['model']['max_tokens'] = max_tokens
     st.session_state['model']['top_p'] = top_p
